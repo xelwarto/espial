e177be574494348251567884eb881834acc13c3c
Ted Elwartowski <xelwarto.pub@gmail.com>
2014-11-19 16:01:10 -0600

    Added basePath

--- lib/espial/apispec.rb
+++ lib/espial/apispec.rb
@@ -1,11 +1,18 @@
 
 module Espial
 	class ApiSpec
-		attr_reader :id
+		attr_reader :id, :paths
 
 		def initialize
-			@swagger = '2.0'
-			@id = nil
+			@swagger 		= '2.0'
+			@id 				= nil
+			@basePath 	= nil
+			@paths 			= []
 		end
+
+		def basePath(arg)
+      @basePath = arg
+      @id = arg
+    end
 	end
 end
