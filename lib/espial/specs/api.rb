894ba47ba40132da6af116639b349fa4191f8e73
Ted Elwartowski <ted@Teds-MacBook-Pro.local>
2014-12-07 20:38:12 -0600

    Added API sepc to specs folder

---
+++ lib/espial/specs/api.rb
@@ -0,0 +1,22 @@
+
+module Espial
+  module Spec
+    class Api < Espial::Spec::Helper
+
+      attr_reader :paths
+      s_attr_accessor :swagger, :basePath, :host
+      a_attr_accessor :schemes, :consumes, :produces
+      s_obj_accessor :info
+      a_obj_accessor :path
+
+      def initialize
+        swagger '2.0'
+      end
+
+      def id
+        @basePath
+      end
+
+    end
+  end
+end
