8642e51a9e9c2912f1ea6b3aca94c171eb29dc4f
Ted Elwartowski <xelwarto.pub@gmail.com>
2015-06-04 15:27:49 -0500

    Updated validation method to match data type

--- lib/espial/controller/helper.rb
+++ lib/espial/controller/helper.rb
@@ -14,8 +14,8 @@ module Espial
     		Espial::Config.instance.specs[espial_get_id]
       end
 
-      def espial_validate(fragment, data)
-        return JSON::Validator.fully_validate(espial_get_spec.to_hash, data, validate_schema: true, fragment: fragment)
+      def espial_validate_json(fragment, json)
+        return JSON::Validator.fully_validate(espial_get_spec.to_hash, json, validate_schema: true, fragment: fragment)
       end
 
     end
